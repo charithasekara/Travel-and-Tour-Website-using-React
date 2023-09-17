2c1981cfddbca15c0f852f6f0e7bb9d2ed67be67
charithasekara <86426242+charithasekara@users.noreply.github.com>
2023-09-18 00:00:57 +0530

    nav bar created

--- travel and tour website/src/App.jsx
+++ travel and tour website/src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+import Head from './Components/Head/Head'
+import Home from './Components/Home/Home'
+import Footer from './Components/Footer/Footer'
 
 function App() {
 
   return (
     <>
-     
+     <Navbar/>
+      {/*<Home/>
+      <Head/>
+  <Footer />*/}
+
     </>
   )
 }
